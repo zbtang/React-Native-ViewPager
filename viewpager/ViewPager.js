7e50b536734f9de70a76a810b7e97c66a5805e90
Leonardo Carvalho <leonardo@geekie.com.br>
2016-12-01 16:47:58 -0200

    Proposal to make pageMargin work on IOS
    This commit simply add styles to ScrollView in order to make pageMaring work.
        * overflow visible makes next border card visible (in case margin is negative)
        * marginHorizontal -pageMargin / 2 makes margin equal balanced on each side of each child

--- viewpager/ViewPager.js
+++ viewpager/ViewPager.js
@@ -24,9 +24,6 @@ export default class ViewPager extends Component {
         onPageScroll: null,
         onPageSelected: null,
         onPageScrollStateChanged: null,
-        /**
-         * iOS not support yet
-         */
         pageMargin: 0
     };
 
@@ -84,11 +81,15 @@ export default class ViewPager extends Component {
             scrollEventThrottle: needMonitorScroll ? ( this.props.onPageScroll ? 8 : 1) : 0,
         };
         if (needMonitorTouch) props = Object.assign(props, this._panResponder.panHandlers);
+        const scrollViewStyle = {
+          overflow: 'visible',
+          marginHorizontal: -this.props.pageMargin / 2,
+        };
         if (this.props.style && !this.props.style.height)
-            return <ScrollView {...props}/>;
+            return <ScrollView {...props} style={[this.props.style, scrollViewStyle]}/>;
         else return (
             <View style={this.props.style}>
-                <ScrollView {...props} style={null}/>
+                <ScrollView {...props} style={scrollViewStyle}/>
             </View>
         );
     }
