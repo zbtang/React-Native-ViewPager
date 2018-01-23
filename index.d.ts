eead29f216f3cb9404b4d0fb7c313fa2806d21e0
a35764 <asheesh@edfora.com>
2018-01-23 18:15:26 +0530

    Making BottomTabBar horizontalscroll property available in typescript

--- index.d.ts
+++ index.d.ts
@@ -17,7 +17,7 @@ declare module 'rn-viewpager' {
       setPageWithoutAnimation(selectedPage: number): void;
     }
 
-    interface IndicatorViewPagerProps extends ViewProperties {
+    interface IndicatorViewPagerProps extends ViewProperties,ViewPagerProps {
       indicator: React.ReactNode;
       pagerStyle?: ViewProperties['style'];
       autoPlayEnable?: boolean;
