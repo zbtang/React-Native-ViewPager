b6b1b299ac4983861a01ec950275f696d91257ed
Hubert Dworczynski <hubert.dworczynski@gmail.com>
2019-09-27 15:55:55 +0200

    add @react-native-community/viewpager as dependency

--- viewpager/ViewPager.js
+++ viewpager/ViewPager.js
@@ -4,7 +4,8 @@
 
 'use strict'
 
-import { PanResponder, Platform, ScrollView, StyleSheet, View, ViewPagerAndroid } from 'react-native'
+import { PanResponder, Platform, ScrollView, StyleSheet, View } from 'react-native'
+import ViewPagerAndroid from "@react-native-community/viewpager";
 import React, { Component } from 'react'
 
 const SCROLLVIEW_REF = 'scrollView'
