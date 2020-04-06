1c64e1012724d02fb2c2830e23ba6eb8c159e143
handi <handimacbook@gmail.com>
2020-04-06 16:14:48 +0700

    update package

--- ios/Pods/Target Support Files/Pods-rnViewPagerHandy/Pods-rnViewPagerHandy-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_rnViewPagerHandy : NSObject
-@end
-@implementation PodsDummy_Pods_rnViewPagerHandy
-@end
--- ios/Pods/Target Support Files/Pods-rnViewPagerHandyTests/Pods-rnViewPagerHandyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_rnViewPagerHandyTests : NSObject
-@end
-@implementation PodsDummy_Pods_rnViewPagerHandyTests
-@end
